b22fd84e1dea5c579c2ed3096ca9a31f9299b2ce
Lysik <Maria.Lysik@pega.com>
2024-12-20 04:29:15 +0100

    Day 19 part 2 - formatting

--- Day19/19.ps1
+++ Day19/19.ps1
@@ -4,16 +4,16 @@ $REGEX_WITH_PATTERNS = "^($($PATTERNS -join '|'))*$"
 
 function Count-WaysToBuildFromPatterns([string]$design) {
   $countUntil = New-Object long[] ($design.Length + 1)
-  $countUntil[0] = 1 # only 1 way to build from pattern of the same length as design
-	for ($i = 1; $i -le $design.Length; $i++) {
-		foreach($pattern in $PATTERNS) {
-			if ($i -ge $pattern.Length -and $design.substring($i-$pattern.Length,$pattern.Length) -eq $pattern) {
-        # this fit times ways to build design so far
-				$countUntil[$i] += $countUntil[$i-$pattern.Length]
-			}
-		}
-	}
-	return $countUntil[$design.Length]
+  $countUntil[0] = 1 # only 1 way to build from a pattern of the same length as design
+  for ($i = 1; $i -le $design.Length; $i++) {
+    foreach($pattern in $PATTERNS) {
+      if ($i -ge $pattern.Length -and $design.substring($i-$pattern.Length,$pattern.Length) -eq $pattern) {
+        # every time a pattern fits, increment current count by the number of ways to build design so far
+        $countUntil[$i] += $countUntil[$i-$pattern.Length]
+      }
+    }
+  }
+  return $countUntil[$design.Length]
 }
 
 $count = 0

50bae542440284a87b6b6525f2a8e3e99c003fdf
Lysik <Maria.Lysik@pega.com>
2024-12-20 01:03:01 +0100

    Day19 part 1

---
+++ Day19/19.ps1
@@ -0,0 +1,10 @@
+﻿$DATA_INPUT = Get-Content C:\Users\lysim\Projects\AdventOfCode2024-PowerShell\Day19\19.txt
+[string[]]$patterns = $DATA_INPUT[0] -split ", "
+$regexPattern = "^($($patterns -join '|'))*$"
+$sum = 0
+for ($i = 2; $i -lt $DATA_INPUT.Count; $i++) {
+  if ($DATA_INPUT[$i] -match $regexPattern) {
+  $sum++
+  }
+}
+Write-Host $sum

95b092ccb1e7658d3cc7e26fadc79b7d5fd23b24
Lysik <Maria.Lysik@pega.com>
2024-12-21 02:24:18 +0100

    Day 20 Part 2

--- Day20/20.ps1
+++ Day20/20.ps1
@@ -1,27 +1,34 @@
 ﻿$DATA_INPUT = Get-Content .\Day20\20.txt
 $MAX_Y = $DATA_INPUT.Length
 $MAX_X = $DATA_INPUT[0].Length
+$OBSTACLE = '#'
 $START = '?'
 $END = '?'
 $TRACK = @{}
 $VISITED = @{}
-$CHEAT_PAUSE = 2
+$CHEAT_PAUSE = 2 # PART 2 - 20
+$TRESHOLD = 100
 
-function Get-NStepNeighbors([int]$x,[int]$y,[int]$n) {
+function Get-NStepNeighbors([int]$x,[int]$y,[int]$max,[int]$min) {
   $neighbors = @{}
-  for ($dx = 0; $dx -le $n; $dx++) {
-    $dy = $n - $dx
-    $neighbors["$($x+$dx),$($y+$dy)"] = $n
-    $neighbors["$($x-$dx),$($y-$dy)"] = $n
-    $neighbors["$($x+$dx),$($y-$dy)"] = $n
-    $neighbors["$($x-$dx),$($y+$dy)"] = $n
+  for ($n = $min; $n -le $max; $n++) {
+    for ($dx = 0; $dx -le $n; $dx++) {
+      $dy = $n - $dx
+      if (($x+$dx) -lt $MAX_X -and ($y+$dy) -lt $MAX_Y -and $DATA_INPUT[$x+$dx][$y+$dy] -ne $OBSTACLE) {
+        $neighbors["$($x+$dx),$($y+$dy)"] = $n
+      }
+      if (($x-$dx) -ge 0 -and ($y-$dy) -ge 0 -and $DATA_INPUT[$x-$dx][$y-$dy] -ne $OBSTACLE) {
+        $neighbors["$($x-$dx),$($y-$dy)"] = $n
+      }
+      if (($x+$dx) -lt $MAX_X -and ($y-$dy) -ge 0 -and $DATA_INPUT[$x+$dx][$y-$dy] -ne $OBSTACLE) {
+        $neighbors["$($x+$dx),$($y-$dy)"] = $n
+      }
+      if (($x-$dx) -ge 0 -and ($y+$dy) -lt $MAX_Y -and $DATA_INPUT[$x-$dx][$y+$dy] -ne $OBSTACLE) {
+        $neighbors["$($x-$dx),$($y+$dy)"] = $n
+      }
+    }
   }
-  return $neighbors.Keys
-}
-
-function Update-Neighbors([string]$key) {
-  $TRACK[$key].next = $TRACK[$key].next | Where-Object { $TRACK[$_] -and $null -eq $VISITED[$_] }
-  $TRACK[$key].jump = $TRACK[$key].jump | Where-Object { $TRACK[$_] -and $null -eq $VISITED[$_] }
+  return $neighbors
 }
 
 for ($y = 0; $y -lt $MAX_Y; $y++) {
@@ -30,11 +37,20 @@ for ($y = 0; $y -lt $MAX_Y; $y++) {
     switch ($DATA_INPUT[$x][$y]) {
       'S' { $START = $fieldKey }
       'E' { $END = $fieldKey }
-      {$_ -ne '#'} {
+      {$_ -ne $OBSTACLE} {
         $TRACK[$fieldKey] = @{} 
-        $TRACK[$fieldKey]['next'] = Get-NStepNeighbors $x $y 1
-        $TRACK[$fieldKey]['jump'] = Get-NStepNeighbors $x $y $CHEAT_PAUSE
+        $TRACK[$fieldKey]['next'] = (Get-NStepNeighbors $x $y 1 1).Keys
+        $TRACK[$fieldKey]['jump'] = Get-NStepNeighbors $x $y $CHEAT_PAUSE 2
       }
+    }
+  }
+}
+
+function Update-Neighbors([string]$key) {
+  $TRACK[$key].next = $TRACK[$key].next | Where-Object { $null -eq $VISITED[$_] }
+  $TRACK[$key].jump.Keys | ForEach-Object {
+    if ($VISITED[$_]) {
+      $TRACK[$key].jump.Remove($_)
     }
   }
 }
@@ -52,12 +68,10 @@ while ($current -ne $END) {
 $VISITED[$END] = $true
 $TRACK[$END]['time'] = $time
 
-$TRESHOLD = 100
 $cheatsOverTresholdCount = 0
 foreach($field in $TRACK.GetEnumerator()) {
-  foreach($jump in $field.Value.jump) {
-    if ($TRACK[$jump].time - $field.Value.time -$CHEAT_PAUSE -ge $TRESHOLD) {
-      Write-Host $field.Key 'jumps to' $jump
+  foreach($jump in $field.Value.jump.GetEnumerator()) {
+    if (($TRACK[$jump.Key].time - $field.Value.time - $jump.Value) -ge $TRESHOLD) {
       $cheatsOverTresholdCount++
     }
   }

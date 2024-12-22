07265b53b9c8a574678fcec8b28f9d6c2a6d00eb
Lysik <Maria.Lysik@pega.com>
2024-12-22 17:08:36 +0100

    Day 22 Part 1

---
+++ Day22/22.ps1
@@ -0,0 +1,20 @@
+﻿$DATA_INPUT = Get-Content .\Day22\22.txt
+$ITERATIONS = 2000
+
+function Get-ProgramOutput([long]$a) {
+  $a = (($a -shl 6) -bxor $a) % 16777216
+  $a = (($a -shr 5) -bxor $a) % 16777216
+  $a = (($a -shl 11) -bxor $a) % 16777216
+  return $a
+}
+
+[long] $sum = 0
+foreach($number in $DATA_INPUT) {
+  [long]$result = [long]$number
+  for($i = 0; $i -lt $ITERATIONS; $i++) {
+    $result = Get-ProgramOutput $result
+  }
+  #Write-Host $number ':' $result
+  $sum += $result
+}
+Write-Host $sum
